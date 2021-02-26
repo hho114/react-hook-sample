a3ce6f4378c082856e6705f7df5a9dadb63d7f75
Alex Ho <huyho114@gmail.com>
2021-02-26 15:33:02 -0500

    update remove and add with hook and loading error

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,29 +1,43 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
 import Search from "./Search";
+import ErrorModal from "../UI/ErrorModal";
 
 function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
-  useEffect(()=>{
-    fetch('https://react-hook-sample-cf6b3-default-rtdb.firebaseio.com/ingredients.json').then(
-      response => response.json()
-    ).then(responseData=>{
-      const loadedIngredients=[];
-      for (const key in responseData) {
-        loadedIngredients.push({
-          id:key,
-          title:responseData[key].title,
-          amount: responseData[key].amount
-        })
-      }
-      setUserIngredients(loadedIngredients);
-    })
-  },[]);
-  const addIngredientHandler = (ingredient) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  useEffect(() => {
     fetch(
-      "https://react-hook-sample-cf6b3-default-rtdb.firebaseio.com/ingredients.json",
+      "https://react-hook-sample-cf6b3-default-rtdb.firebaseio.com/ingredients.json"
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log("Rendering ingredients", userIngredients);
+  }, [userIngredients]);
+
+  const filterIngredientsHandler = useCallback((filterIngredients) => {
+    setUserIngredients(filterIngredients);
+  }, []);
+  const addIngredientHandler = (ingredient) => {
+    setIsLoading(true);
+    fetch(
+      `https://react-hook-sample-cf6b3-default-rtdb.firebaseio.com/ingredients.json`,
       {
         method: "POST",
         body: JSON.stringify(ingredient),
@@ -31,28 +45,46 @@ function Ingredients() {
       }
     )
       .then((response) => {
+        setIsLoading(false);
         return response.json();
       })
       .then((responseData) => {
         setUserIngredients((prevIngredients) => [
           ...prevIngredients,
-          { id : responseData.name, ...ingredient },
+          { id: responseData.name, ...ingredient },
         ]);
       });
   };
 
   const removeIngredientHandler = (ingredientId) => {
-    setUserIngredients((prevIngredients) =>
-      prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
-    );
+    setIsLoading(true);
+    fetch(
+      `https://react-hook-sample-cf6b3-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`,
+      {
+        method: "DELETE",
+      }
+    ).then((response) => {
+      setIsLoading(false)
+      setUserIngredients((prevIngredients) =>
+        prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
+      );
+    }).catch(error =>{
+      setError('something went wrong!');
+    });
   };
+
+  const clearError = ()=>{
+    setError(null);
+    setIsLoading(false);
+  }
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading}/>
 
       <section>
-        <Search />
+        <Search onLoadingIngredients={filterIngredientsHandler} />
         <IngredientList
           ingredients={userIngredients}
           onRemoveItem={removeIngredientHandler}
